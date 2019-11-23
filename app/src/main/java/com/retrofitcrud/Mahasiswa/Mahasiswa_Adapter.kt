46700c876ebe7bd8b878fc1fdc9c2070ef65bf68
Lina H <lina.handayani06@gmail.com>
2019-11-23 14:45:10 +0700

    layout delete button

---
+++ app/src/main/java/com/retrofitcrud/Mahasiswa/Mahasiswa_Adapter.kt
@@ -0,0 +1,13 @@
+package com.retrofitcrud.Mahasiswa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.retrofitcrud.R
+
+class Mahasiswa_Adapter : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mahasiswa__adapter)
+    }
+}
